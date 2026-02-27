7d230e14d8887bba77d7407dc23f9b3483e13929
souha <souha.guezguez@gmail.com>
2026-02-28 00:48:18 +0100

    modification tp4+tp5

---
+++ DashboardData/Program.cs
@@ -0,0 +1,28 @@
+using DashboardData.Components;
+using DashboardData.Services;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+//Enregistrement du service avec une durée de vie "Scoped"S
+builder.Services.AddScoped<ISensorService, SensorService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
